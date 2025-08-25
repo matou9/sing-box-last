543d1358868a4c12059704f8054ddff13ac47edb
david_feng <385297@qq.com>
2025-08-25 11:22:20 +0800

    20250825

---
+++ experimental/libbox/command.go
@@ -0,0 +1,21 @@
+package libbox
+
+const (
+	CommandLog int32 = iota
+	CommandStatus
+	CommandServiceReload
+	CommandServiceClose
+	CommandCloseConnections
+	CommandGroup
+	CommandSelectOutbound
+	CommandURLTest
+	CommandGroupExpand
+	CommandClashMode
+	CommandSetClashMode
+	CommandGetSystemProxyStatus
+	CommandSetSystemProxyEnabled
+	CommandGroupInfoOnly
+	CommandConnections
+	CommandCloseConnection
+	CommandGetDeprecatedNotes
+)
